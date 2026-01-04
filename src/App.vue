630ae8f548e40a3033364a0fdb65cc50d5f1819b
magicFeirl <2100709458@qq.com>
2026-01-04 19:21:40 +0800

    chore: update alert

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,15 @@ export default {
   name: 'App',
   components: {
     BottomNav
+  },
+  created() {
+    if (true || location.hostname == '101.132.142.130') {
+      this.$alert('服务器即将到期，请访问新网址：https://514810.xyz/', '提示', {
+        confirmButtonText: '确定'
+      }).then(() => {
+        window.open('http://514810.xyz/', '_blank')
+      })
+    }
   }
 }
 </script>
